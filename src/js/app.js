13be2c09ba186ae373df59163cf503423ce4d421
whats0n <mrdvv@mail.ru>
2017-03-03 23:40:52 +0200

    update

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,13 @@
-import sayHello from './lib/sayHello.js';
+import slick from 'slick-carousel';
 
-sayHello();
+
+$(document).ready(function() {
+	const slider = $('.js-slider');
+	slider.slick({
+		prevArrow: '<button class="slider__arrow slider__prev"></button>',
+		nextArrow: '<button class="slider__arrow slider__next"></button>',
+		dotsClass: 'slider__dots',
+		dots: true,
+		fade: true
+	});
+});
